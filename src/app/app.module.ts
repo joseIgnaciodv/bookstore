5e02cc4295ffa75872ab3c900ba90285e69d83e9
joseIgnaciodv <joseignaciodelvallebustillo@gmail.com>
2022-09-30 08:31:08 +0200

    changes to overall structure of project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { MainComponent } from './components/main/main.component';
 import { CatalogComponent } from './components/catalog/catalog.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainComponent,
     CatalogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
