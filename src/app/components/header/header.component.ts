a991c179679ee103fc5210d36aa455d6772b67f8
joseIgnaciodv <joseignaciodelvallebustillo@gmail.com>
2022-10-04 11:01:58 +0200

    added theme button and corresponding functionality

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TitleStrategy } from '@angular/router';
+import { AppComponent } from 'src/app/app.component';
 
 @Component({
   selector: 'app-header',
@@ -7,14 +8,37 @@ import { TitleStrategy } from '@angular/router';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  img_url: string = "night-mode-dark.png"
-  theme: string = "Dark"
-  light_clicked: boolean = true
-  dark_clicked: boolean = false
+  
+  img_url:string = ""
+  theme:string = ""
+  dark_theme:boolean = false
+  burger_menu: boolean = false
 
-  constructor() { }
+  constructor(public app: AppComponent) { }
+
+  changeTheme(){
+    this.app.dark_theme = !this.app.dark_theme
+    if(this.app.dark_theme == true){
+      this.img_url = "brightness.png"
+      this.theme = "Light"
+    }else{
+      this.img_url = "night-mode-dark.png"
+      this.theme = "Dark"
+    }
+    this.dark_theme = this.app.dark_theme
+  }
+
+  openSideNav(){
+    this.burger_menu = true
+  }
+
+  closeSideNav(){
+    this.burger_menu = false
+  }
 
   ngOnInit(): void {
+    this.img_url = "night-mode-dark.png"
+    this.theme = "Dark"
   }
 
 }
